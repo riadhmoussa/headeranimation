d440614e8148c21b7e4da41deaff56b7e6a42f31
Hassine Feker <faker@wi-mobi.com>
2021-11-07 08:40:43 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,174 @@
+import React, { useRef } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  StatusBar,
+  Platform,
+} from 'react-native';
+import HeaderImageScrollView, {
+  TriggeringView,
+} from 'react-native-image-header-scroll-view';
+
+import * as Animatable from 'react-native-animatable';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const MIN_HEIGHT = Platform.OS === 'ios' ? 90 : 55;
+const MAX_HEIGHT = 350;
+
+const App = ({ route }) => {
+  const itemData = {
+    id: '1',
+    coordinate: {
+      latitude: 22.6293867,
+      longitude: 88.4354486,
+    },
+    title: 'Amazing Food Place',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque est metus, gravida vel ex volutpat, posuere euismod tortor. Pellentesque tincidunt, mi ac varius blandit, quam orci dignissim risus, vitae rutrum orci urna ut neque.
+      
+      Pellentesque condimentum ut libero id blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`,
+    image: "https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/244421397_902907970639693_943100453836784578_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=-I9ryNbUuH4AX_G2Osc&_nc_ht=scontent.ftun9-1.fna&oh=3eddd8b7be201be67c418102a70db74c&oe=618CA8D2",
+    rating: 4,
+    reviews: 99,
+    categories: ['Restaurant', 'Hotel', 'Dineout'],
+  }
+  const navTitleView = useRef(null);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <HeaderImageScrollView
+        maxHeight={MAX_HEIGHT}
+        minHeight={MIN_HEIGHT}
+        maxOverlayOpacity={0.6}
+        minOverlayOpacity={0.3}
+        renderHeader={() => (
+          <Image source={{uri:itemData.image}} style={styles.image} />
+        )}
+        renderForeground={() => (
+          <View style={styles.titleContainer}>
+            <Text style={styles.imageTitle}>Hello Wordl!!!</Text>
+          </View>
+        )}
+        renderFixedForeground={() => (
+          <Animatable.View style={styles.navTitleView} ref={navTitleView}>
+            <Text style={styles.navTitle}>{itemData.title}</Text>
+          </Animatable.View>
+        )}>
+        <TriggeringView
+          style={styles.section}
+          onHide={() => navTitleView.current.fadeInUp(200)}
+          onDisplay={() => navTitleView.current.fadeOut(100)}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={styles.title}>Overview</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'flex-end' }}>
+              <FontAwesome name="star" size={16} color="#FF6347" />
+              <Text style={{ marginHorizontal: 2 }}>{itemData.rating}</Text>
+              <Text>({itemData.reviews})</Text>
+            </View>
+          </View>
+        </TriggeringView>
+        <View style={[styles.section, styles.sectionLarge]}>
+          <Text style={styles.sectionContent}>{itemData.description}</Text>
+        </View>
+
+        <View style={styles.section}>
+          <View style={styles.categories}>
+            {itemData.categories.map((category, index) => (
+              <View style={styles.categoryContainer} key={index}>
+                <FontAwesome name="tag" size={16} color="#fff" />
+                <Text style={styles.category}>{category}</Text>
+              </View>
+            ))}
+          </View>
+        </View>
+
+        <View style={[styles.section, { height: 250 }]}>
+         
+        </View>
+      </HeaderImageScrollView>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    height: MAX_HEIGHT,
+    width: Dimensions.get('window').width,
+    alignSelf: 'stretch',
+    resizeMode: 'cover',
+  },
+  title: {
+    fontSize: 20,
+  },
+  name: {
+    fontWeight: 'bold',
+  },
+  section: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+    backgroundColor: 'white',
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  sectionContent: {
+    fontSize: 16,
+    textAlign: 'justify',
+  },
+  categories: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    flexWrap: 'wrap',
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#FF6347',
+    borderRadius: 20,
+    margin: 10,
+    padding: 10,
+    paddingHorizontal: 15,
+  },
+  category: {
+    fontSize: 14,
+    color: '#fff',
+    marginLeft: 10,
+  },
+  titleContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageTitle: {
+    color: 'white',
+    backgroundColor: 'transparent',
+    fontSize: 24,
+  },
+  navTitleView: {
+    height: MIN_HEIGHT,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: Platform.OS === 'ios' ? 40 : 5,
+    opacity: 0,
+  },
+  navTitle: {
+    color: 'white',
+    fontSize: 18,
+    backgroundColor: 'transparent',
+  },
+  sectionLarge: {
+    minHeight: 300,
+  },
+});
